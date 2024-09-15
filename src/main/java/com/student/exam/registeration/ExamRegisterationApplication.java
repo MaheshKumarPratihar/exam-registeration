aca9d2a352371ab24389ac2bf6f26e51fdbeb01d
Mahesh Kumar <62428991+MaheshKumarPratihar@users.noreply.github.com>
2024-09-15 15:56:19 +0530

    initial commit

---
+++ src/main/java/com/student/exam/registeration/ExamRegisterationApplication.java
@@ -0,0 +1,13 @@
+package com.student.exam.registeration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamRegisterationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamRegisterationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/student/exam/registeration/ExamRegisterationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.exam.registeration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamRegisterationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
